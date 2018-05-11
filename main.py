56fbc374ec2e9b9ec7c2b9479b68a73f9eee29c6
Marc Evrard <marc.evrard@gmail.com>
2018-05-11 17:08:42 +0900

    Refactor main.py

--- main.py
+++ main.py
@@ -11,42 +11,46 @@ import torch.onnx
 import data
 import model
 
-parser = argparse.ArgumentParser(description='PyTorch Wikitext-2 RNN/LSTM Language Model',
+
+SEP_THIN = '-' * 89
+SEP_THICK = '=' * 89
+
+parser = argparse.ArgumentParser(description="PyTorch Wikitext-2 RNN/LSTM Language Model",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--data', type=str, default='./data/wikitext-2',
-                    help='location of the data corpus')
+                    help="location of the data corpus")
 parser.add_argument('--model', type=str, default='LSTM',
-                    help='type of recurrent net (RNN_TANH, RNN_RELU, LSTM, GRU)')
+                    help="type of recurrent net (RNN_TANH, RNN_RELU, LSTM, GRU)")
 parser.add_argument('--emsize', type=int, default=200,
-                    help='size of word embeddings')
+                    help="word embeddings' dimension")
 parser.add_argument('--nhid', type=int, default=200,
-                    help='number of hidden units per layer')
+                    help="number of hidden units per layer")
 parser.add_argument('--nlayers', type=int, default=2,
-                    help='number of layers')
+                    help="number of layers")
 parser.add_argument('--lr', type=float, default=20,
-                    help='initial learning rate')
+                    help="initial learning rate")
 parser.add_argument('--clip', type=float, default=0.25,
-                    help='gradient clipping')
+                    help="gradient clipping")
 parser.add_argument('--epochs', type=int, default=40,
-                    help='upper epoch limit')
-parser.add_argument('--batch_size', type=int, default=20, metavar='N',
-                    help='batch size')
-parser.add_argument('--bptt', type=int, default=35,
-                    help='sequence length')
+                    help="upper epoch limit")
+parser.add_argument('--batch-size', type=int, default=20, metavar='N',
+                    help="batch size")
+parser.add_argument('--bptt', type=int, default=35,  # BPTT: Backprop. thru time?
+                    help="sequence length")
 parser.add_argument('--dropout', type=float, default=0.2,
-                    help='dropout applied to layers (0 = no dropout)')
+                    help="dropout applied to layers (0 = no dropout)")
 parser.add_argument('--tied', action='store_true',
-                    help='tie the word embedding and softmax weights')
+                    help="tie the word embedding and softmax weights")
 parser.add_argument('--seed', type=int, default=1111,
-                    help='random seed')
+                    help="random seed")
 parser.add_argument('--cuda', action='store_true',
-                    help='use CUDA')
+                    help="use CUDA")
 parser.add_argument('--log-interval', type=int, default=200, metavar='N',
-                    help='report interval')
+                    help="report interval")
 parser.add_argument('--save', type=str, default='./models/model.pt',
-                    help='path to save the final model')
+                    help="path to save the final model")
 parser.add_argument('--onnx-export', type=str, default='',
-                    help='path to export the final model in onnx format')
+                    help="path to export the final model in onnx format")
 args = parser.parse_args()
 
 # Set the random seed manually for reproducibility.
@@ -75,30 +79,33 @@ corpus = data.Corpus(args.data)
 # dependence of e. g. 'g' on 'f' can not be learned, but allows more efficient
 # batch processing.
 
-def batchify(_data, bsz):
-    # Work out how cleanly we can divide the dataset into bsz parts.
-    nbatch = _data.size(0) // bsz
+def batchify(data_in, size):
+    '''Work out how to cleanly divide the dataset into batches.'''
+    batchs_n = data_in.size(0) // size
     # Trim off any extra elements that wouldn't cleanly fit (remainders).
-    _data = _data.narrow(0, 0, nbatch * bsz)
-    # Evenly divide the data across the bsz batches.
-    _data = _data.view(bsz, -1).t().contiguous()
-    return _data.to(device)
+    data_cln = data_in.narrow(0, 0, batchs_n * size)
+    # Evenly divide the data across the size batches.
+    data_batches = data_cln.view(size, -1).t().contiguous()
+    return data_batches.to(device)
 
 eval_batch_size = 10
 train_data = batchify(corpus.train, args.batch_size)
 val_data = batchify(corpus.valid, eval_batch_size)
 test_data = batchify(corpus.test, eval_batch_size)
 
+# print(corpus.valid.shape)
+# print(val_data.shape)
+
 ###############################################################################
 # Build the model
 ###############################################################################
 
-ntokens = len(corpus.dictionary)
-model = model.RNNModel(args.model, ntokens, args.emsize, args.nhid,
-                       args.nlayers, args.dropout, args.tied).to(device)
+model = model.RNNModel(rnn_type=args.model, ntoken=len(corpus.dictionary),
+                       ninp=args.emsize, nhid=args.nhid, nlayers=args.nlayers,
+                       dropout=args.dropout, tie_weights=args.tied).to(device)
 
 # AttributeError: 'DataParallel' object has no attribute 'init_hidden'
-# model = torch.nn.DataParallel(model)
+# model = torch.nn.DataParallel(model)  # **MEV**
 
 criterion = nn.CrossEntropyLoss()
 
@@ -107,7 +114,7 @@ criterion = nn.CrossEntropyLoss()
 ###############################################################################
 
 def repackage_hidden(h):
-    """Wraps hidden states in new Tensors, to detach them from their history."""
+    '''Wraps hidden states in new Tensors, to detach them from their history.'''
     if isinstance(h, torch.Tensor):
         return h.detach()
     else:
@@ -119,49 +126,45 @@ def repackage_hidden(h):
 # a bptt-limit of 2, we'd get the following two Variables for i = 0:
 # ┌ a g m s ┐ ┌ b h n t ┐
 # └ b h n t ┘ └ c i o u ┘
-# Note that despite the name of the function, the subdivison of data is not
-# done along the batch dimension (i.e. dimension 1), since that was handled
+# Note that despite the name of the function, the subdivision of data is not
+# done along the batch dimension (i.e., dimension 1), since that was handled
 # by the batchify function. The chunks are along dimension 0, corresponding
 # to the seq_len dimension in the LSTM.
 
-def get_batch(source, i):
-    seq_len = min(args.bptt, len(source) - 1 - i)
-    _data = source[i:i+seq_len]
-    target = source[i+1:i+1+seq_len].view(-1)
-    return _data, target
+def get_batch(data_batches, i):
+    seq_len = min(args.bptt, len(data_batches) - 1 - i)
+    data_seq = data_batches[i: i+seq_len]
+    target = data_batches[i+1 : i+1+seq_len].view(-1)
+    return data_seq, target
 
 
-def evaluate(data_source):
-    # Turn on evaluation mode which disables dropout.
-    model.eval()
+def evaluate(data_batches):
+    model.eval()    # Turn on evaluation mode, which disables dropout.
     total_loss = 0.
-    _ntokens = len(corpus.dictionary)
     hidden = model.init_hidden(eval_batch_size)
-    with torch.no_grad():
-        for i in range(0, data_source.size(0) - 1, args.bptt):
-            _data, targets = get_batch(data_source, i)
-            output, hidden = model(_data, hidden)
-            output_flat = output.view(-1, _ntokens)
-            total_loss += len(_data) * criterion(output_flat, targets).item()
+    with torch.no_grad():   # no tracking history
+        for i in range(0, data_batches.size(0) - 1, args.bptt):
+            data_seq, targets = get_batch(data_batches, i)
+            output, hidden = model(data_seq, hidden)
+            output_flat = output.view(-1, len(corpus.dictionary))
+            total_loss += len(data_seq) * criterion(output_flat, targets).item()
             hidden = repackage_hidden(hidden)
-    return total_loss / len(data_source)
+    return total_loss / len(data_batches)
 
 
 def train():
-    # Turn on training mode which enables dropout.
-    model.train()
+    model.train()  # Turn on training mode, which enables dropout.
     total_loss = 0.
     start_time = time.time()
-    _ntokens = len(corpus.dictionary)
     hidden = model.init_hidden(args.batch_size)
     for batch, i in enumerate(range(0, train_data.size(0) - 1, args.bptt)):
-        _data, targets = get_batch(train_data, i)
+        data_seq, targets = get_batch(train_data, i)
         # Starting each batch, we detach the hidden state from how it was previously produced.
         # If we didn't, the model would try backpropagating all the way to start of the dataset.
         hidden = repackage_hidden(hidden)
         model.zero_grad()
-        output, hidden = model(_data, hidden)
-        loss = criterion(output.view(-1, _ntokens), targets)
+        output, hidden = model(data_seq, hidden)
+        loss = criterion(output.view(-1, len(corpus.dictionary)), targets)
         loss.backward()
 
         # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
@@ -174,10 +177,9 @@ def train():
         if batch % args.log_interval == 0 and batch > 0:
             cur_loss = total_loss / args.log_interval
             elapsed = time.time() - start_time
-            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
-                  'loss {:5.2f} | ppl {:8.2f}'.format(
-                      epoch, batch, len(train_data) // args.bptt, lr,
-                      elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
+            print(f'| epoch {epoch:3d} | {batch:5d}/{len(train_data) // args.bptt:5d} batches '
+                  f'| lr {lr:02.2f} | ms/batch {elapsed * 1000 / args.log_interval:5.2f} '
+                  f'| loss {cur_loss:5.2f} | ppl {math.exp(cur_loss):8.2f}')
             total_loss = 0
             start_time = time.time()
 
@@ -202,11 +204,10 @@ try:
         epoch_start_time = time.time()
         train()
         val_loss = evaluate(val_data)
-        print('-' * 89)
-        print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
-              'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
-                                         val_loss, math.exp(val_loss)))
-        print('-' * 89)
+        print(SEP_THIN)
+        print(f'| end of epoch {epoch:3d} | time: {time.time() - epoch_start_time:5.2f}s'
+              f'| valid loss {val_loss:5.2f} | valid ppl {math.exp(val_loss):8.2f}')
+        print(SEP_THIN)
         # Save the model if the validation loss is the best we've seen so far.
         if not best_val_loss or val_loss < best_val_loss:
             with open(args.save, 'wb') as f:
@@ -216,7 +217,7 @@ try:
             # Anneal the learning rate if no improvement has been seen in the validation dataset.
             lr /= 4.0
 except KeyboardInterrupt:
-    print('-' * 89)
+    print(SEP_THIN)
     print('Exiting from training early')
 
 # Load the best saved model.
@@ -228,10 +229,9 @@ with open(args.save, 'rb') as f:
 
 # Run on test data.
 test_loss = evaluate(test_data)
-print('=' * 89)
-print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
-    test_loss, math.exp(test_loss)))
-print('=' * 89)
+print(SEP_THICK)
+print(f'| End of training | test loss {test_loss:5.2f} | test ppl {math.exp(test_loss):8.2f}')
+print(SEP_THICK)
 
 if args.onnx_export:
     # Export the model in ONNX format.
